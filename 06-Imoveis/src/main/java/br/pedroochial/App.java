d81b06e8f72c63c37d816cd2daad34b06447fd36
PedroOchial <pedro.ochial@edu.pucrs.br>
2026-09-09 12:58:45 +0000

    criacao da claas movem e geo e comeco da residencial

---
+++ 06-Imoveis/src/main/java/br/pedroochial/App.java
@@ -0,0 +1,9 @@
+package br.pedroochial;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "Hello, world! Projeto 06-Imoveis criado com sucesso.");
+    }
+}
