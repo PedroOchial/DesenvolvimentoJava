77882ec63ae07cacc151aa312fc76b8b8c4ba5d4
PedroOchial <pedro.ochial@edu.pucrs.br>
2026-09-14 01:55:19 +0000

    finalixacao da class calcula imposto

--- 06-Imoveis/src/main/java/br/pedroochial/Imovel.java
+++ 06-Imoveis/src/main/java/br/pedroochial/Imovel.java
@@ -3,35 +3,41 @@ package br.pedroochial;
 public abstract class Imovel {
 
     private String nomeProprietario;
-private int areaConstruida;
-private Geo geoLocalizacao;
-private static double vr;
+    private int areaConstruida;
+    private Geo geoLocalizacao;
+    private static double vr;
 
-public abstract double calculaImposto();
+    public abstract double calculaImposto();
 
-public Imovel (String nomeProprietario, int areaConstruida, Geo geoLocalizacao, double vr ){
-    this.nomeProprietario = nomeProprietario;
-    this.areaConstruida = areaConstruida;
-    this.geoLocalizacao = geoLocalizacao;
-    this.vr = vr;
-}
+    public Imovel(String nomeProprietario, int areaConstruida, Geo geoLocalizacao) {
+        this.nomeProprietario = nomeProprietario;
+        this.areaConstruida = areaConstruida;
+        this.geoLocalizacao = geoLocalizacao;
+    }
 
-public String getNomeProprietario(){
-    return this.nomeProprietario;
-}
-public int getAreaConstruida(){
-    return this.areaConstruida;
-}
-public Geo getGeoLocalizacao(){
-    return this.geoLocalizacao;
-}
-public double getVr(){
-    return this.vr;
-}
-public String toString(){
-return "Nome Proprietario " + this.nomeProprietario +
-        "Area Construida " + this.areaConstruida +
-        " Geo Localizacao" + this.geoLocalizacao;
+    public String getNomeProprietario() {
+        return this.nomeProprietario;
+    }
 
- }
+    public int getAreaConstruida() {
+        return this.areaConstruida;
+    }
+
+    public Geo getGeoLocalizacao() {
+        return this.geoLocalizacao;
+    }
+
+    public static double getVr() {
+        return vr;
+    }
+
+    public static void setVr(double valor) {
+        vr = valor;
+    }
+
+    public String toString() {
+        return "Nome Proprietario " + this.nomeProprietario +
+                " Area Construida " + this.areaConstruida +
+                " Geo Localizacao " + this.geoLocalizacao;
+    }
 }
--- 06-Imoveis/src/main/java/br/pedroochial/Residencial.java
+++ 06-Imoveis/src/main/java/br/pedroochial/Residencial.java
@@ -5,21 +5,22 @@ public class Residencial extends Imovel {
     private int areaTerreno;
 
     public Residencial (String nomeProprietario, int areaConstruida, Geo geoLocalizacao, int areaTerreno, double vr){
-        super( nomeProprietario, areaConstruida, geoLocalizacao,vr);
+        super( nomeProprietario, areaConstruida, geoLocalizacao);
         this.areaTerreno = areaTerreno;
     }
-    @Override
-    public double calculaImposto(){
-        if (getAreaConstruida()  < 200){
-            return 0;
-        }
-        else if (getAreaConstruida() >= 200 && getAreaConstruida() < 1000 ){
-            return ((areaTerreno - 200) * getVr());
-        }
 
-        else  
-         return ((areaTerreno - 200) *  getVr() *100);
 
+@Override
+public double calculaImposto(){
+    if (getAreaConstruida() <= 200) {
+        return 0;
     }
 
+    double imposto = (getAreaConstruida() - 200) * Imovel.getVr();
+
+    if (areaTerreno > 1000) {
+        imposto = imposto + (100 * Imovel.getVr());
     }
+
+    return imposto;
+}
